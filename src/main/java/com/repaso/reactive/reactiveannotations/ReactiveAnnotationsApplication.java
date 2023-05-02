0d5241e9764e4fee87596d255ac421102925db98
Danik616 <ddanielvargas2011@gmail.com>
2023-05-02 10:32:48 -0500

    Se hace la inicializacion del proyecto

---
+++ src/main/java/com/repaso/reactive/reactiveannotations/ReactiveAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.repaso.reactive.reactiveannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveAnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repaso/reactive/reactiveannotations/ReactiveAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repaso.reactive.reactiveannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
